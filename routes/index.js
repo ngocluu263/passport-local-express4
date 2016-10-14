7796d85a7df0ee1d4495f0c88e705c1ba83d1e58
luunguyen <luunguyen@ccintegration.com>
2016-10-14 16:04:03 +0700

    Update

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.get('/register', function(req, res) {
 });
 
 router.post('/register', function(req, res, next) {
-    Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+    Account.register(new Account({ username : req.body.username }), req.body.password, req.body.full_name, function(err, account) {
         if (err) {
           return res.render('register', { error : err.message });
         }
